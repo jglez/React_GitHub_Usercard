e87ed979227fa739b3d66440de4ac728330b23c4
jglez <jglez.cs@gmail.com>
2021-01-12 19:43:25 -0600

    Cleaned boilerplate, created class component, created state for card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import { URL, usernames } from './data.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      img: '',
+      id: ''
+    }
+  }
+
+
+  render() {
+    // Schedule code inside of the callback to run after the first render of
+    // the component lifecycle. 
+    // useEffect(() => {
+    //   axios
+    //     .get(`${URL}${usernames[0]}`)
+    //     .then(res => {
+    //       this.setState({
+    //         name: res.data.name
+    //       })
+    //     })
+    //     .catch(err => {
+    //       console.log('no bueno')
+    //     })
+    // }, [])
+
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>GitHub Usercard</h1>
+          <h2>Name: {this.state.name}</h2>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const usernames = [
+  'bigknell',
+  'ladrillo',
+  'geohot',
+  'clementmihailescu',
+  'kubowania'
+]
+
+export const URL = 'https://api.github.com/users/'
+
+
+
